548fc591b5f2090826c50b17a42320f41b464f7f
Nelson Green <nelson.green@gmail.com>
2022-12-28 09:38:02 -0600

    DataLog JS data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -25,6 +27,12 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // Start DataLogManager recording of on-robot telemetry
+    DataLogManager.start();
+
+    // Record joystick and control values from DriverStation
+    DriverStation.startDataLog(DataLogManager.getLog());
+
     // Configure default commands and condition bindings on robot startup
     m_robot.configureBindings();
   }
